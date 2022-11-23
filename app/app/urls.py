9c8c9a4f62e4c86867198e12a42aea8a8f262939
Sergio A S <sergio-abu@proton.me>
2022-11-23 17:22:27 -0300

    health check

--- app/app/urls.py
+++ app/app/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from core import views as core_views
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
@@ -33,6 +34,7 @@ urlpatterns = [
     ),
     path('api/user/', include('user.urls')),
     path('api/recipe/', include('recipe.urls')),
+    path('api/health-check/', core_views.health_check, name='health-check'),
 ]
 
 if settings.DEBUG:
